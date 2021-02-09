095b93b9914d2293d531f629794b0917a2b2a8a6
pratikshazen <77830882+pratikshazen@users.noreply.github.com>
2021-02-09 21:16:43 +0530

    Add files via upload

---
+++ assign/AssigConStateCity/ConStateCity.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ConStateCity.aspx.cs" Inherits="AssigConStateCity.ConStateCity" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 40px;
+        }
+        .auto-style2 {
+            margin-left: 360px;
+        }
+    </style>
+</head>
+<body style="margin-bottom: 56px">
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            Country:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DDL1_Country" runat="server" AutoPostBack="True" DataTextField="Country_Name" OnSelectedIndexChanged="DDL1_Country_SelectedIndexChanged">
+            </asp:DropDownList>
+        </div>
+        <p class="auto-style1">
+            &nbsp;</p>
+        <p class="auto-style1">
+            State&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :<asp:DropDownList ID="DDL2_State" runat="server" AutoPostBack="True" DataTextField="State_Name" OnSelectedIndexChanged="DDL2_State_SelectedIndexChanged">
+            </asp:DropDownList>
+        </p>
+        <p class="auto-style1">
+            &nbsp;</p>
+        <p class="auto-style1">
+            City&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :<asp:DropDownList ID="DDL3_City" runat="server" AutoPostBack="True" DataTextField="City_Name" OnSelectedIndexChanged="DDL3_City_SelectedIndexChanged">
+            </asp:DropDownList>
+        </p>
+        <p class="auto-style2">
+            <asp:Button ID="btnOk" runat="server" Text="Ok" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ assign/AssigConStateCity/ConStateCity.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AssigConStateCity
+{
+    public partial class ConStateCity : System.Web.UI.Page
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ToString());
+
+        public void Country()
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select Country_ID,Country_Name from CountryTab", cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            DDL1_Country.DataSource = dr;
+       
+            DDL1_Country.Items.Add("Select Country");
+            DDL1_Country.DataTextField = "Country_Name";
+            DDL1_Country.DataValueField = "Country_ID";
+            DDL1_Country.DataBind();
+            cn.Close();
+        }
+        public void State()
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select State_ID,State_Name from StateTab where Country_ID='" + DDL1_Country.SelectedValue + "'", cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            DDL2_State.DataSource = dr;
+            DDL2_State.Items.Add("Select State");
+            DDL2_State.DataTextField = "State_Name";
+            DDL2_State.DataValueField = "State_ID";
+            DDL2_State.DataBind();
+            cn.Close();
+        }
+
+        public void City()
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select City_ID,City_Name from CityTab where State_ID='" + DDL2_State.SelectedValue + "'", cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DDL3_City.DataSource = dr;
+            DDL3_City.Items.Add("Select City");
+            DDL3_City.DataTextField = "City_Name";
+            DDL3_City.DataValueField = "City_ID";
+            DDL3_City.DataBind();
+            cn.Close();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Country();
+            }
+        }
+        protected void DDL1_Country_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            State();
+        }
+
+        protected void DDL2_State_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            City();
+        }
+
+        protected void DDL3_City_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
